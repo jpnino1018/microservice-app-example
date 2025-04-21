fb585d2332f888e30761e2c9208ae4fdb3784398
Yuluka Gigante Muriel <yulukeitor@gmail.com>
2025-04-20 23:48:26 -0500

    feat: add descriptions to outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,19 +1,24 @@
 output "resource_group" {
-  value = azurerm_resource_group.microservices_rg.name
+  description = "Name of the created Azure Resource Group"
+  value       = azurerm_resource_group.microservices_rg.name
 }
 
 output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server
+  description = "Login server URL for the Azure Container Registry (ACR)"
+  value       = azurerm_container_registry.acr.login_server
 }
 
 output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks_cluster.name
+  description = "Name of the Azure Kubernetes Service (AKS) cluster"
+  value       = azurerm_kubernetes_cluster.aks_cluster.name
 }
 
 output "redis_hostname" {
-  value = azurerm_redis_cache.redis_queue.hostname
+  description = "Hostname of the Azure Redis Cache instance"
+  value       = azurerm_redis_cache.redis_queue.hostname
 }
 
 output "apim_gateway_url" {
-  value = azurerm_api_management.apim.gateway_url
+  description = "Gateway URL for the Azure API Management instance"
+  value       = azurerm_api_management.apim.gateway_url
 }
