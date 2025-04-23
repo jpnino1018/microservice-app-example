224e791d2ff268bc55aa5097af74b3fa303e0910
Yuluka Gigante <71479323+yuluka@users.noreply.github.com>
2025-04-23 01:01:28 -0500

    Update Todos.vue

--- frontend/src/components/Todos.vue
+++ frontend/src/components/Todos.vue
@@ -6,7 +6,7 @@
       <div class="row">
         <div class="col-sm-12 text-left">
         <h1>
-          List of TODOs
+          List of Pending Tasks
           <transition name="fade">
             <small v-if="total">({{ total }})</small>
           </transition>
